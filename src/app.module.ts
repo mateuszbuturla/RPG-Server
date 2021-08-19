d38869d8ebb2c339aa280c23fe7618e5c81c491f
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-08-19 17:54:06 +0200

    fix/database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Connection } from 'typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -20,4 +21,6 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
